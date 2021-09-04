6657bfc295997277ebeb08f7bb181792d63bf372
kk-no <kenokohe@gmail.com>
2021-09-05 05:34:00 +0900

    add service to type query

--- main.go
+++ main.go
@@ -38,12 +38,12 @@ func main() {
 func parse(r io.Reader) (*plugin.CodeGeneratorRequest, error) {
 	b, err := io.ReadAll(r)
 	if err != nil {
-		return nil, fmt.Errorf("parse/ReadAll: %w", err)
+		return nil, err
 	}
 
 	var req plugin.CodeGeneratorRequest // do not pointer
 	if err := proto.Unmarshal(b, &req); err != nil {
-		return nil, fmt.Errorf("parse/Unmarshal: %w", err)
+		return nil, err
 	}
 	return &req, nil
 }
@@ -59,17 +59,47 @@ func process(req *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, e
 	for _, name := range req.GetFileToGenerate() {
 		var content strings.Builder
 
-		messageTypes := files[name].GetMessageType()
+		f := files[name]
+
+		// Service
+		serviceTypes := f.GetService()
+		services := make([]string, 0, len(serviceTypes))
+
+		services = append(services, "type Query {") // TODO: Implement other operations such as Mutation.
+		for _, serviceType := range serviceTypes {
+			services = append(services, indent+"# "+serviceType.GetName())
+			for _, method := range serviceType.GetMethod() {
+				in := pop(strings.Split(method.GetInputType(), "."))
+				out := pop(strings.Split(method.GetOutputType(), "."))
+				services = append(services, fmt.Sprintf("%s%s(req: %s): %s", indent, method.GetName(), in, out))
+			}
+			log.Printf("%s: %+v", serviceType.GetName(), serviceType)
+		}
+		services = append(services, "}\n\n")
+
+		if _, err := content.WriteString(strings.Join(services, "\n")); err != nil {
+			return nil, err
+		}
+
+		// Message
+		messageTypes := f.GetMessageType()
 		messages := make([]string, 0, len(messageTypes))
 
 		for _, messageType := range messageTypes {
-			if fields := messageType.GetField(); len(fields) != 0 {
+			fields := messageType.GetField()
+			if strings.Contains(messageType.GetName(), "Request") {
+				messages = append(messages, "input "+messageType.GetName()+" {")
+			} else {
 				messages = append(messages, "type "+messageType.GetName()+" {")
+			}
+			if len(fields) != 0 {
 				for _, field := range fields {
 					messages = append(messages, indent+field.GetName()+": "+types.GQL[field.GetType()])
 				}
-				messages = append(messages, "}\n")
+			} else {
+				messages = append(messages, indent+"_: Boolean # noop field")
 			}
+			messages = append(messages, "}\n")
 			log.Printf("%s: %+v", messageType.GetName(), messageType)
 		}
 
@@ -90,10 +120,15 @@ func process(req *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, e
 func emit(res *plugin.CodeGeneratorResponse) error {
 	buf, err := proto.Marshal(res)
 	if err != nil {
-		return fmt.Errorf("emit/Marsgal: %w", err)
+		return err
 	}
 	if _, err := os.Stdout.Write(buf); err != nil {
-		return fmt.Errorf("emit/Write: %w", err)
+		return err
 	}
 	return nil
+}
+
+// pop extracts the end value of the slice.
+func pop(s []string) string {
+	return s[len(s)-1]
 }
